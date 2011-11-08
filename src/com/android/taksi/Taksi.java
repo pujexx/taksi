175c26515e7ccf99d50684164b10255860e261de
puji rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-11-08 23:36:30 +0700

    comit ke 2

--- src/com/android/taksi/Taksi.java
+++ src/com/android/taksi/Taksi.java
@@ -1,76 +1,159 @@
 package com.android.taksi;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
+import android.R.integer;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class Taksi extends Activity {
 	static final int DATE_DIALOG_ID = 0;
-	/** Called when the activity is first created. */
+	static final int TIME_DIALOG_ID = 1;
+	private int mYear;
+	private int mMonth;
+	private int mDay;
+	private int mHour;
+	private int mMinute;
 	String url = "http://10.0.2.2/taksi/index.php/api/post";
-	private EditText nama,alamat,tanggal,waktu,nope,tujuan,jumlah;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        nama =(EditText)findViewById(R.id.nama);
-        alamat = (EditText)findViewById(R.id.alamat);
-        tanggal = (EditText)findViewById(R.id.tanggal);
-        waktu = (EditText)findViewById(R.id.waktu);
-        nope =(EditText)findViewById(R.id.nope);
-        tujuan = (EditText)findViewById(R.id.tujuan);
-        jumlah = (EditText)findViewById(R.id.jumlah);
-    tanggal.setOnClickListener(new OnClickListener() {
-		
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
+	private EditText nama, alamat, tanggal, waktu, nope, tujuan, jumlah;
+    private Button send,reset;
+	private ProgressDialog dialog; 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	
+		nama = (EditText) findViewById(R.id.nama);
+		alamat = (EditText) findViewById(R.id.alamat);
+		tanggal = (EditText) findViewById(R.id.tanggal);
+		waktu = (EditText) findViewById(R.id.waktu);
+		nope = (EditText) findViewById(R.id.nope);
+		tujuan = (EditText) findViewById(R.id.tujuan);
+		jumlah = (EditText) findViewById(R.id.jumlah);
+		send =(Button)findViewById(R.id.send);
+		reset = (Button)findViewById(R.id.reset);
+		tanggal.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+		waktu.setOnClickListener(new OnClickListener() {
 			
-		showDialog(DATE_DIALOG_ID);	
-		}
-	});
-    }
-    
-    @Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			  showDialog(TIME_DIALOG_ID);	
+			}
+		});
+		send.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//dialog.show(Taksi.this,"Prosesing","Prosesing");
+				getRequest();
+			}
+		});
+		final Calendar c = Calendar.getInstance();
+		mYear = c.get(Calendar.YEAR);
+		mMonth = c.get(Calendar.MONTH);
+		mDay = c.get(Calendar.DAY_OF_MONTH);
+		mHour = c.get(Calendar.HOUR_OF_DAY);
+		mMinute = c.get(Calendar.MINUTE);
+	}
+
+	@Override
 	protected Dialog onCreateDialog(int id) {
 		// TODO Auto-generated method stub
-    	switch(id){
-    	case DATE_DIALOG_ID :
-    		return new DatePickerDialog(Taksi.this,null, 2011, 1, 1);
-    	    	}
+		switch (id) {
+		case DATE_DIALOG_ID :
+	
+			return new DatePickerDialog(Taksi.this, setTanggal, mYear,mMonth , mDay);
+			
+	
+		case TIME_DIALOG_ID :
+	
+			return new TimePickerDialog(Taksi.this,setWaktu,mHour,mMinute,true);
+		
+	
+			
+		}
+	
 		return null;
 	}
 
-	public void getRequest(EditText txtResult){
-        Log.d("getRequest",url);
-         HttpClient client = new DefaultHttpClient();
-         HttpPost request = new HttpPost(url);
-       
-         try{
-        	 List post = new ArrayList(1);
-             HttpResponse response = client.execute(request);
-             //
-             //txtResult.setText(Koneksi.request(response));
-         }catch(Exception ex){
-             //txtResult.setText("Failed Connect to server!");
-         }
+	private DatePickerDialog.OnDateSetListener setTanggal = new OnDateSetListener() {
 
-     }
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			tanggal.setText(Integer.toString(dayOfMonth)+"-"+Integer.toString(monthOfYear+1)+"-"+Integer.toString(year));
+
+		}
+	};
+	private TimePickerDialog.OnTimeSetListener setWaktu = new OnTimeSetListener() {
+		
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			// TODO Auto-generated method stub
+			waktu.setText(Integer.toString(hourOfDay)+":"+Integer.toString(minute));
+		}
+	};
+
+	@SuppressWarnings({ "unchecked", "static-access" })
+	public void getRequest() {
+		Log.d("getRequest", url);
+		HttpClient client = new DefaultHttpClient();
+		HttpPost request = new HttpPost(url);
+
+		try {
+			List post = new ArrayList(1);
+		
+			post.add(new BasicNameValuePair("nama", nama.getText().toString()));
+			post.add(new BasicNameValuePair("alamat", alamat.getText().toString()));
+			post.add(new BasicNameValuePair("tanggal", tanggal.getText().toString()));
+			post.add(new BasicNameValuePair("waktu", waktu.getText().toString()));
+			post.add(new BasicNameValuePair("nope", nope.getText().toString()));
+			post.add(new BasicNameValuePair("tujuan", tujuan.getText().toString()));
+			post.add(new BasicNameValuePair("jumlah", jumlah.getText().toString()));
+			
+			request.setEntity(new UrlEncodedFormEntity(post));		
+		
+		
+			HttpResponse response = client.execute(request);
+			
+			
+			Toast.makeText(Taksi.this, Koneksi.request(response), Toast.LENGTH_LONG).show();
+			//
+			// txtResult.setText(Koneksi.request(response));
+		} catch (Exception ex) {
+			// txtResult.setText("Failed Connect to server!");
+		}
+
+	}
 }
